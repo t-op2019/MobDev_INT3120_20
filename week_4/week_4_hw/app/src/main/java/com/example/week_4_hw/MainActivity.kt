3a611d61915f9bdf36d2c98428bce70124c180ab
Tin Du Vo <55805983+t-op2019@users.noreply.github.com>
2023-09-13 11:39:40 +0700

    submit w4 homework

--- week_4/week_4_hw/app/src/main/java/com/example/week_4_hw/MainActivity.kt
+++ week_4/week_4_hw/app/src/main/java/com/example/week_4_hw/MainActivity.kt
@@ -7,16 +7,25 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.selection.selectableGroup
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.Checkbox
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.week_4_hw.components.AppTopBar
@@ -48,11 +57,18 @@ class MainActivity : ComponentActivity() {
 fun AppScaffold() {
 	val cheeses = listOf("Cheese", "2 x Cheese", "None")
 	val shapes = listOf("Square", "Round")
+	val toppings = listOf("Pepperoni", "Mushroom", "Veggies")
 	val (selectedCheese, setSelectedCheese) = remember {
 		mutableStateOf("Cheese")
 	}
 	val (selectedShape, setSelectedShape) = remember {
 		mutableStateOf("Square")
+	}
+	val (selectedToppings, setSelectedToppings) = remember {
+		mutableStateOf(listOf<String>())
+	}
+	val (order, setOrder) = remember {
+		mutableStateOf("")
 	}
 	Scaffold(
 		topBar = { AppTopBar() },
@@ -86,6 +102,40 @@ fun AppScaffold() {
 					)
 				}
 			}
+			Row(modifier = Modifier.selectableGroup()) {
+				toppings.map { topping ->
+					Row(verticalAlignment = Alignment.CenterVertically) {
+						Checkbox(
+							checked = selectedToppings.contains(topping),
+							onCheckedChange = {
+								if (selectedToppings.contains(topping)) {
+									setSelectedToppings(selectedToppings - topping)
+								} else {
+									setSelectedToppings(selectedToppings + topping)
+								}
+							}
+						)
+						Text(text = topping)
+					}
+				}
+			}
+
+			Text(
+				text = "Your ordering",
+				fontSize = MaterialTheme.typography.titleLarge.fontSize,
+				fontWeight = MaterialTheme.typography.titleLarge.fontWeight,
+				color = Color.Gray
+			)
+			TextField(
+				modifier = Modifier
+					.fillMaxWidth()
+					.height(240.dp),
+				value = order,
+				onValueChange = { value -> setOrder(value) },
+				singleLine = false,
+				maxLines = 9,
+				keyboardOptions = KeyboardOptions(imeAction = ImeAction.Done)
+			)
 
 			BottomNav()
 		}
