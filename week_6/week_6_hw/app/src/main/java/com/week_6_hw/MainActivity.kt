dd5e46e69586e69b3aec6f98449eebeb321d338d
Tin Du Vo <55805983+t-op2019@users.noreply.github.com>
2023-09-20 13:14:05 +0700

    submit week 6 hw

--- week_6/week_6_hw/app/src/main/java/com/week_6_hw/MainActivity.kt
+++ week_6/week_6_hw/app/src/main/java/com/week_6_hw/MainActivity.kt
@@ -3,13 +3,36 @@ package com.week_6_hw
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.combinedClickable
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.MoreVert
+import androidx.compose.material3.DropdownMenu
+import androidx.compose.material3.DropdownMenuItem
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.ColorFilter
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.week_6_hw.ui.theme.Purple40
 import com.week_6_hw.ui.theme.Week_6_hwTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,25 +45,73 @@ class MainActivity : ComponentActivity() {
 					modifier = Modifier.fillMaxSize(),
 					color = MaterialTheme.colorScheme.background
 				) {
-					Greeting("Android")
+					AppScaffold()
 				}
 			}
 		}
 	}
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-	Text(
-		text = "Hello $name!",
-		modifier = modifier
-	)
-}
-
 @Preview(showBackground = true)
+@OptIn(ExperimentalMaterial3Api::class, ExperimentalFoundationApi::class)
 @Composable
-fun GreetingPreview() {
-	Week_6_hwTheme {
-		Greeting("Android")
+fun AppScaffold() {
+	val openContext = remember { mutableStateOf(false) }
+	val openMenu = remember { mutableStateOf(false) }
+	val contextActions = listOf("Copy image", "Save image")
+	val menuActions = listOf("Action 1", "Action 2", "Action 3")
+	Scaffold(topBar = {
+		TopAppBar(
+			title = { Text(text = "Menus") },
+			actions = {
+				IconButton(
+					onClick = { openMenu.value = true },
+				) {
+					// creates a menu icon
+					Image(
+						imageVector = Icons.Filled.MoreVert, contentDescription = "More vert",
+						colorFilter = ColorFilter.tint(Color.White)
+					)
+				}
+				DropdownMenu(
+					expanded = openMenu.value,
+					onDismissRequest = { openMenu.value = false }) {
+					menuActions.map { action ->
+						DropdownMenuItem(
+							text = { Text(text = action) },
+							onClick = { openMenu.value = false })
+					}
+				}
+			},
+			colors = TopAppBarDefaults.mediumTopAppBarColors(
+				containerColor = Purple40,
+				titleContentColor = Color.White
+			)
+		)
+	}) {
+		Column(
+			modifier = Modifier
+				.padding(it)
+				.padding(10.dp)
+				.fillMaxSize(),
+			horizontalAlignment = Alignment.CenterHorizontally
+		) {
+			Text(text = "Long press the image for extra context")
+			Row {
+				Image(painter = painterResource(id = R.drawable.dog), contentDescription = "A dog",
+					modifier = Modifier.combinedClickable(onLongClick = {
+						openContext.value = true
+					}) { })
+				DropdownMenu(
+					expanded = openContext.value,
+					onDismissRequest = { openContext.value = false }) {
+					contextActions.map { action ->
+						DropdownMenuItem(
+							text = { Text(text = action) },
+							onClick = { openContext.value = false })
+					}
+				}
+			}
+		}
 	}
 }
